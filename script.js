6001c601e5af9dec726b9e21ff6407aa7fc1b3d3
Jacob Winkler <Jacob.Winkler@oracle.com>
2025-07-12 07:49:30 -0500

    added js to call service to get joke

---
+++ script.js
@@ -0,0 +1,35 @@
+const generateButton = document.getElementById('generateButton')
+const joke = document.getElementById('joke');
+
+function queryJoke (){
+    const xhr = new XMLHttpRequest();
+    // const url = 'https://api.chucknorris.io/jokes/random';
+
+   xhr.open('GET', 'https://api.chucknorris.io/jokes/random', true);
+
+
+    xhr.onreadystatechange = function () {
+
+        if(this.readyState ===4){
+                if(this.status === 200){
+                    const data = JSON.parse(this.responseText);
+
+                   joke.innerText=data.value;
+                }else{
+                    console.log('not funny');
+                }
+        }
+        
+        }
+         xhr.send();  
+}
+
+
+
+
+
+
+
+
+
+generateButton.addEventListener('click', queryJoke);
